bd7483a7241bbe259ba46ba1b9041098a80a8334
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2024-08-22 01:49:07 -0300

    Revisão dos comentários.

--- python/fluxo-de-execucao-condicional/sistema-bancario-basico.py
+++ python/fluxo-de-execucao-condicional/sistema-bancario-basico.py
@@ -10,23 +10,23 @@ if conta == "858585-8":
         print("4 - Sair")
         opcao = int(input("Informe a opção: "))
         saldo = 500
-        if opcao == 1:                                                  #Saldo
-            print("Seu saldo é de R$", saldo)
-            print(f"Seu saldo é de R$ {saldo:.2f}")
-        elif opcao == 2:                                                #Saque
+        if opcao == 1:                                          # Operação de Saldo
+            print("Seu saldo é de R$", saldo)                           # Impressão sem formatação (Opção 1)
+            print(f"Seu saldo é de R$ {saldo:.2f}")                     # Impressão formatada para moeda (Opção 2)
+        elif opcao == 2:                                        # Operação de Saque
             saque = float(input("Informe o valor do saque "))
             if saque <= saldo:
                 saldo = saldo - saque
-                print("Saldo atualizado R$", saldo)
-                print(f"Saldo atualizado R$ {saldo:.2f}")
+                print("Saldo atualizado R$", saldo)                     # Impressão sem formatação (Opção 1)
+                print(f"Saldo atualizado R$ {saldo:.2f}")               # Impressão formatada para moeda (Opção 2)
             else:
                 print("Saldo indisponível")
-        elif opcao == 3:                                                #Deposito
+        elif opcao == 3:                                        # Operação de Depósito
             deposito = float(input("Informe o valor do deposito: "))
             saldo = saldo + deposito
-            print("Saldo atualizado R$", saldo)
-            print(f"Saldo atualizado R$ {saldo:.2f}")
-        elif opcao == 4:                                                # Sair
+            print("Saldo atualizado R$", saldo)                         # Impressão sem formatação (Opção 1)
+            print(f"Saldo atualizado R$ {saldo:.2f}")                   # Impressão formatada para moeda (Opção 2)
+        elif opcao == 4:                                         # Operação Sair
             print("Obrigado por usar nosso banco.")
         else: #Inválido
             print("Opção inválida.")
--- python/fluxo-de-execucao-condicional/sistema-caixa-de-loja.py
+++ python/fluxo-de-execucao-condicional/sistema-caixa-de-loja.py
@@ -6,14 +6,14 @@ print("2 - a prazo")
 
 opcao = int(input("Opção: "))
 if opcao == 1:
-    desconto = valor_compra * 0.1       #10% é equivalente ao decimal 0,1
+    desconto = valor_compra * 0.1                                   # 10% é equivalente ao decimal 0,1
     valor_a_pagar = valor_compra - desconto
-    print(f"Valor a pagar R$ {valor_a_pagar:.2f}")   #Resultado com f-string
-    print("Valor a pagar R$", valor_a_pagar)       #Resultado com string
+    print(f"Valor a pagar R$ {valor_a_pagar:.2f}")                  # Resultado com f-string (Opção 1)
+    print("Valor a pagar R$", valor_a_pagar)                        # Resultado com string (Opção 2)
 elif opcao == 2:
     qtde_parcelas = int(input("Informe o número de parcelas"))
     valor_parcela = valor_compra / qtde_parcelas
-    print("{qtde_parcelas} parcelas de R$ {valor_parcela:.2f}")     #Resultado com f-string
-    print(qtde_parcelas, "parcelas de R$", valor_parcela)           #Resultado com string
+    print("{qtde_parcelas} parcelas de R$ {valor_parcela:.2f}")     # Resultado com f-string (Opção 1)
+    print(qtde_parcelas, "parcelas de R$", valor_parcela)           # Resultado com string (Opção 2)
 else:
     print("Opção inválida")
